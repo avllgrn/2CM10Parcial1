b728652e35ad476f139cb842d8feb4d5a819953b
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-02-13 12:45:46 -0600

    Modificar una posicion en la lista

--- listas.py
+++ listas.py
@@ -1,21 +1,6 @@
 from os import system
 from random import randrange
 
-def unaFuncionConInput():
-    V = []
-    print(V, type(V))
-
-    n = int(input('Cuantos datos? '))
-
-    for i in range(n):
-        V.append(float(input(f'Ingresa dato {i} ')))
-        print(V, type(V), len(V))
-
-    for i in range(len(V)):
-        print(i, V[i], type(V[i]))
-
-    print(V, type(V), len(V))
-
 def unaFuncionConRandrange():
     V = []
     print(V, type(V))
@@ -24,17 +9,18 @@ def unaFuncionConRandrange():
 
     for i in range(n):
         V.append(randrange(60,81))
-        print(V, type(V), len(V))
 
     for i in range(len(V)):
         print(i, V[i], type(V[i]))
 
     print(V, type(V), len(V))
 
+    posicion = int(input('Cual posicion? '))
+    V[posicion] = -123
+    print(V, type(V), len(V))
+
 if __name__ == '__main__':
     system('cls')
-    
-    unaFuncionConInput()
-    print()
+
     unaFuncionConRandrange()
     print()
