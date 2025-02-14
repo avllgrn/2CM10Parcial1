dbf22c5f3c8d129fff4ffce4a69a9d213835928c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-02-14 12:35:02 -0600

    suma, promedio, menor y mayor en lista

--- listas.py
+++ listas.py
@@ -3,19 +3,57 @@ from random import randrange
 
 if __name__ == '__main__':
     system('cls')
-
+    V = []
     n = int(input('Dame n '))
 
-    s = 0
-    for i in range(n+1):
-        dato = randrange(1,5)
-        s = s+dato
-        print(f'dato = {dato}\ts = {s}')
-    print(f'\ns = {s}\n')
+    for i in range(n):
+        V.append(randrange(-100, 101))
 
-    p = 1
-    for i in range(1, n+1):
-        dato = randrange(1,5)
-        p = p*dato
-        print(f'dato = {dato}\tp = {p}')
-    print(f'\np = {p}')
+    for i in range(n):
+        print(f'V[{i}] = {V[i]}')
+    print()
+    
+    s = 0
+    for i in range(n):
+        s = s + V[i]
+        #print(f'V[{i}] = {V[i]}\ts = {s}')
+
+    print(f'\ns = {s}')
+    if n>0:
+        promedio = s/n
+        print(f'promedio = {promedio}')
+
+    menor = V[0]
+    posMenor = 0
+    for i in range(n):
+       #print(f'menor = {menor}, = V[{posMenor}]')
+       if V[i] < menor:
+           menor = V[i]
+           posMenor = i
+    print(f'menor = {menor}, = V[{posMenor}]')
+
+
+    mayor = V[0]
+    posMayor = 0
+    for i in range(n):
+       #print(f'mayor = {mayor}, = V[{posMayor}]')
+       if V[i] > mayor:
+           mayor = V[i]
+           posMayor = i
+    print(f'mayor = {mayor}, = V[{posMayor}]')
+
+    menoresQuePromedio = 0
+    for i in range(n):
+        if V[i] < promedio:
+            menoresQuePromedio += 1
+    print(f'Hay {menoresQuePromedio} menores que el promedio')
+
+
+    mayorQuePromedio = 0
+    for i in range(n):
+        if V[i] > promedio:
+            mayorQuePromedio += 1
+    print(f'Hay {mayorQuePromedio} mayores que el promedio')
+
+
+    
