6c42a96a19f7d670c271e34ca93c3ab40cd1d47d
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-02-14 11:49:56 -0600

    Suma y multiplicacion acumuladas

--- listas.py
+++ listas.py
@@ -1,26 +1,18 @@
 from os import system
-from random import randrange
-
-def unaFuncionConRandrange():
-    V = []
-    print(V, type(V))
-
-    n = int(input('Cuantos datos? '))
-
-    for i in range(n):
-        V.append(randrange(60,81))
-
-    for i in range(len(V)):
-        print(i, V[i], type(V[i]))
-
-    print(V, type(V), len(V))
-
-    posicion = int(input('Cual posicion? '))
-    V[posicion] = -123
-    print(V, type(V), len(V))
 
 if __name__ == '__main__':
     system('cls')
 
-    unaFuncionConRandrange()
-    print()
+    n = int(input('Dame n '))
+
+    s = 0
+    for i in range(n+1):
+        s = s+i
+        print(f'i = {i}\ts = {s}')
+    print(f'\ns = {s}\n')
+
+    p = 1
+    for i in range(1, n+1):
+        p = p*i
+        print(f'i = {i}\tp = {p}')
+    print(f'\np = {p}')
